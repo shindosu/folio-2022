08bf383d94b0d29aaed45bbfe9f82c14cb315946
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2022-08-07 12:02:19 +0900

    delete eslint-disable

--- src/models/Scene.js
+++ src/models/Scene.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
