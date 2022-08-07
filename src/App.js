c23c8c1a420f4aaabb5d15655ff90392c6b5fe44
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2022-08-07 23:36:55 +0900

    disable for mobile

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './modules/prototypeMethods';
 import Canvas from './Canvas';
 import Panels from './components/panels/Panels';
 import LoadingBar from './components/LoadingBar';
+import NotReady from './components/NotReady';
 
-const App = () => (
+const App = () => (['iPhone', 'Android', 'iPad'].some(term => window.navigator.userAgent.indexOf(term) >= 0) ? <NotReady /> : (
   <>
     <LoadingBar />
     <Panels />
     <Canvas />
   </>
-);
+));
 
 export default App;
---
+++ src/components/NotReady.jsx
@@ -0,0 +1,3 @@
+const NotReady = () => <div>Sorry not available for mobile devices yet.</div>;
+
+export default NotReady;
