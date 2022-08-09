a47aa983c2d35cd8b53b6886f960ec82a4fe513f
Taisei Yamada <taiseiyamadashindosu@gmail.com>
2022-08-09 21:20:20 +0900

    dynamic application environment for contentful

--- src/modules/contentful.js
+++ src/modules/contentful.js
@@ -3,7 +3,7 @@ const contentful = require('contentful');
 const client = contentful.createClient({
   space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
   accessToken: process.env.REACT_APP_ENV_CONTENTFUL_ACCESS_TOKEN,
-  environment: 'master'
+  environment: process.env.REACT_APP_APPLICATION_ENVIRONMENT
 });
 
 const contentType = text => {
